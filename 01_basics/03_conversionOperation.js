a93ac5140010bb5f3f6ecccbddfee23947681256
NIKHIL SHARMA <ss6141057@gmail.com>
2026-07-03 16:24:33 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "nikhil"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "44" => 44
+// "44abc" => NaN (not a number)
+// true => 1; false => 0;
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false; 
+// "nikhil" => true
+
+let someNumber = "33"
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof someNumber);
